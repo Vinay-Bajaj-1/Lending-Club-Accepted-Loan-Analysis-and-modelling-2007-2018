d80b24e8d7cd2c0cadcc5ea75a675fe0ec3c7917
Vinay Bajaj <vinaybajaj157@gmail.com>
2023-12-03 15:49:09 +0530

    Modify

--- FrontEnd.py
+++ FrontEnd.py
@@ -28,7 +28,7 @@ def submit_config(user_data):
     else:
         col2.write("The Customer will not be able to repay the loan") 
     
-    col2.metric('Model Prediction %', formatted_res, str(formatted_res-50) + "%")
+    col2.metric('Model Prediction %', formatted_res, formatted_res-50)
     
     
 
