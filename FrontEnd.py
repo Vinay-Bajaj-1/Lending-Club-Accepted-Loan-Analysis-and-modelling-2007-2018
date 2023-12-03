9d611cb3927574e842b6666cae0ac88b62deb4bb
Vinay Bajaj <vinaybajaj157@gmail.com>
2023-12-03 12:06:50 +0530

    modified

--- FrontEnd.py
+++ FrontEnd.py
@@ -79,30 +79,30 @@ def main():
 
     # Get user input for various fields
     loan_amnt = st.sidebar.number_input("Loan Amount ($)", min_value=0, step=1)
-    term = st.selectbox("Term", ["36 months", "60 months"])
-    int_rate = st.slider("Interest Rate (%)", min_value=0.0, max_value=20.0, step=0.01)
-    installment = st.number_input("Installment ($)", min_value=0, step=1)
-    sub_grade = st.selectbox("Sub Grade", ["A1", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4", "B5",
+    term = st.sidebar.selectbox("Term", ["36 months", "60 months"])
+    int_rate = st.sidebar.slider("Interest Rate (%)", min_value=0.0, max_value=20.0, step=0.01)
+    installment = st.sidebar.number_input("Installment ($)", min_value=0, step=1)
+    sub_grade = st.sidebar.selectbox("Sub Grade", ["A1", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4", "B5",
                                            "C1", "C2", "C3", "C4", "C5", "D1", "D2", "D3", "D4", "D5",
                                            "E1", "E2", "E3", "E4", "E5", "F1", "F2", "F3", "F4", "F5",
                                            "G1", "G2", "G3", "G4", "G5"])
-    home_ownership = st.selectbox("Home Ownership", ["MORTAGE", "RENT", "OWN", "OTHER"])
-    annual_inc = st.number_input("Annual Income ($)", min_value=0, step=1)
-    verification_status = st.selectbox("Verification Status", ["Source Verified", "Verified"])
-    purpose = st.selectbox("Purpose", ["Credit Card", "Debt Consolidation", "Home Improvement", "House",
+    home_ownership = st.sidebar.selectbox("Home Ownership", ["MORTAGE", "RENT", "OWN", "OTHER"])
+    annual_inc = st.sidebar.number_input("Annual Income ($)", min_value=0, step=1)
+    verification_status = st.sidebar.selectbox("Verification Status", ["Source Verified", "Verified"])
+    purpose = st.sidebar.selectbox("Purpose", ["Credit Card", "Debt Consolidation", "Home Improvement", "House",
                                        "Major Purchase", "Educational", "Medical", "Moving", "Renewable Energy",
                                        "Small Business", "Wedding", "Vacation", "Other"])
-    dti = st.number_input("Debt-to-Income Ratio", min_value=0.0, step=0.01)
-    earliest_cr_line = st.number_input("Earliest Credit Line Year", min_value=0, step=1)
-    open_acc = st.number_input("Open Accounts", min_value=0, step=1)
-    pub_rec = st.number_input("Public Records", min_value=0, step=1)
-    revol_bal = st.number_input("Revolving Balance ($)", min_value=0, step=1)
-    revol_util = st.number_input("Revolving Utilization Rate (%)", min_value=0.0, max_value=100.0, step=0.01)
-    total_acc = st.number_input("Total Accounts", min_value=0, step=1)
-    initial_list_status = st.selectbox("Initial List Status", ["W", "T"])
-    application_type = st.selectbox("Application Type", ["Joint", "Individual"])
-    mort_acc = st.number_input("Mortgage Accounts", min_value=0, step=1)
-    pub_rec_bankruptcies = st.number_input("Public Record Bankruptcies", min_value=0, step=1)
+    dti = st.sidebar.number_input("Debt-to-Income Ratio", min_value=0.0, step=0.01)
+    earliest_cr_line = st.sidebar.number_input("Earliest Credit Line Year", min_value=0, step=1)
+    open_acc = st.sidebar.number_input("Open Accounts", min_value=0, step=1)
+    pub_rec = st.sidebar.number_input("Public Records", min_value=0, step=1)
+    revol_bal = st.sidebar.number_input("Revolving Balance ($)", min_value=0, step=1)
+    revol_util = st.sidebar.number_input("Revolving Utilization Rate (%)", min_value=0.0, max_value=100.0, step=0.01)
+    total_acc = st.sidebar.number_input("Total Accounts", min_value=0, step=1)
+    initial_list_status = st.sidebar.selectbox("Initial List Status", ["W", "T"])
+    application_type = st.sidebar.selectbox("Application Type", ["Joint", "Individual"])
+    mort_acc = st.sidebar.number_input("Mortgage Accounts", min_value=0, step=1)
+    pub_rec_bankruptcies = st.sidebar.number_input("Public Record Bankruptcies", min_value=0, step=1)
 
     
     user_data = pd.DataFrame({
