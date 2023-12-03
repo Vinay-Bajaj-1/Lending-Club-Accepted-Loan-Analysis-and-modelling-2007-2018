c50ac71cdc512d9446b4f9c3c483f712f4260b97
Vinay Bajaj <vinaybajaj157@gmail.com>
2023-12-03 22:31:28 +0530

    all file

--- FrontEnd.py
+++ FrontEnd.py
@@ -33,11 +33,11 @@ def submit_config(user_data):
     col4.metric('Logistic Regression',str(res_lr) + '%', res_lr - 75)
     col5.metric('Random Forest', str(res_rf) + '%', res_rf - 75)
     
-    col2.bar_chart({'ANN' : [res_ann],
+    '''col2.bar_chart({'ANN' : [res_ann],
                     'Logistic Regression' : [res_lr],
                     'Random Forest' : [res_rf]},
                   color=["#00589C", "#1891C3","#50E3C2"]
-                  )
+                  )'''
     
     
     
